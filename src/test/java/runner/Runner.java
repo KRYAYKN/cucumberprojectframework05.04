26e6c36e0309a60563c32c934d80eeea9289f689
KRYAYKN <korayayakin@gmail.com>
2023-04-05 23:06:47 +0300

    Initial commit

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,24 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features="src/test/resources/features",
+        glue= {"stepdefinitions", "hooks"},
+        tags="@Techprotest"  ,
+
+        dryRun= false
+
+)
+
+
+
+
+
+public class Runner {
+
+}
---
+++ src/test/java/stepdefinitions/Techprostepdef.java
@@ -0,0 +1,30 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import static org.junit.Assert.assertTrue;
+
+public class Techprostepdef {
+    @Given("{string} sayfasina gidilir")
+    public void sayfasina_gidilir(String string) {
+        Driver.getDriver().get(ConfigReader.getProperty(string));
+    }
+    @When("sayfa basligi yazdirilir")
+    public void sayfa_basligi_yazdirilir() {
+        System.out.println(Driver.getDriver().getTitle());
+    }
+    @Then("sayfa basliginda {string} oldugu dogrulanir")
+    public void sayfa_basliginda_oldugu_dogrulanir(String string) {
+     assertTrue(Driver.getDriver().getTitle().contains(string));
+        }
+
+    @Then("sayfa kapatilir")
+    public void sayfa_kapatilir() {
+Driver.closeDriver();
+    }
+
+}
