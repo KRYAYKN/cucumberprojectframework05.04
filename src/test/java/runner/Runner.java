a007df0869cb9e2e54f3e7207a766f0004c8ca14
Koray Ayakin <117303724+KRYAYKN@users.noreply.github.com>
2023-05-13 13:13:26 +0300

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -6,6 +6,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+                plugin = {
+                "pretty",//raporlarin daha okunakli olmasi icin
+                "html:TestOutput/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+//                "rerun:target/failedRerun.txt",
+//                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        monochrome = true,
 
         features="src/test/resources/features",
         glue= {"stepdefinitions", "hooks"},
